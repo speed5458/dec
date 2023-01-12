e8e87fd2c3a5e749343005a84e08615167d2ebad
최영준 <44596010+speed5458@users.noreply.github.com>
2023-01-12 16:26:58 +0900

    초기셋팅

---
+++ src/main/java/com/dream/dec/DecApplication.java
@@ -0,0 +1,13 @@
+package com.dream.dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/dec/DecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
