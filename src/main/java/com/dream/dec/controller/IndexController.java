b34403e0a0b58b26f9bd18d6f3b6e8dd67988928
최영준 <44596010+speed5458@users.noreply.github.com>
2023-02-28 12:59:31 +0900

    metadata public image 생성

--- src/main/java/com/dream/dec/controller/IndexController.java
+++ src/main/java/com/dream/dec/controller/IndexController.java
@@ -1,17 +1,34 @@
 package com.dream.dec.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
-//
-//    @GetMapping(value = "/")
-//    public String Home() {
-//        return "index";
-//    }
+
+    @GetMapping(value = "/image/meta")
+    @ResponseBody
+    public ResponseEntity<Resource> showImage() throws MalformedURLException {
+        Resource resource = new ClassPathResource("static/img/photo/facility/facility-img4.jpeg");
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .cacheControl(CacheControl.noCache())
+                .header(HttpHeaders.CONTENT_DISPOSITION)
+                .body(resource);
+    }
 
 
 }
